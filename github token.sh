8b56d72c807df1a671aee55fb1f7586407d7a298
Zhaochen (Melody) Zhan <zhanzhaochen@foxmail.com>
2017-12-19 11:33:34 -0500

    Update github token.sh

--- github token.sh
+++ github token.sh
@@ -92,3 +92,19 @@ git clean -f
 
 # Delete both remote and local branch 
 git push -d <remote_name> <branch_name>
+
+
+#If you want to revert changes made to your working copy, do this:
+git checkout .
+
+#If you want to revert changes made to the index (i.e., that you have added), do this. Warning this will reset all of your unpushed commits to master!:
+git reset
+
+#If you want to revert a change that you have committed, do this:
+git revert <commit 1> <commit 2>
+
+#If you want to remove untracked files (e.g., new files, generated files):
+git clean -f
+
+#Or untracked directories (e.g., new or automatically generated directories):
+git clean -fd
